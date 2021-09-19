ffd802e39d0fe476e3cc620939ae5735dbafe908
Electra Tapanari <etapanari@gmail.com>
2021-09-19 10:48:54 +0200

    Adding versioning to the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
 Rails.application.routes.draw do
+  
   root 'pages#index'
 
-  namespace :api do 
-    resources :contacts, only: [:show, :index, :destroy, :update, :create] do
-      member do
-        get "changes", to: "contacts#changes"
-      end
+  namespace :api do
+
+    namespace :v1 do
+      resources :contacts, only: [:show, :index, :destroy, :update, :create] do
+        member do
+          get "changes", to: "contacts#changes"
+        end
+      end        
     end
+    
+    namespace :v2 do
+      resources :contacts, only: [:show, :index, :destroy, :update, :create] do
+        member do
+          get "changes", to: "contacts#changes"
+        end
+      end        
+    end
+
   end
+
   get '*path', to: 'pages#index', via: :all
 end
